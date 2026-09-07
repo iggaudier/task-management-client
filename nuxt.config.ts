bddd0b171c41a055ae306fa32db16e4210abf732
iggaudier <ggaudier.ian@gmail.com>
2026-09-07 22:31:50 +0800

    chore: added nuxt module and tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    '@nuxtjs/tailwindcss'
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
+    },
+  },
 })
